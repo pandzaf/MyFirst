ecd8c67251df94800e242aed36419018c18fef18
PandhuDtr <pandhudtr@gmail.com>
2020-04-07 13:29:56 +0700

    OKE

--- MyFirstGithub/Program.cs
+++ MyFirstGithub/Program.cs
@@ -6,7 +6,7 @@ namespace MyFirstGithub
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!!");
         }
     }
 }
